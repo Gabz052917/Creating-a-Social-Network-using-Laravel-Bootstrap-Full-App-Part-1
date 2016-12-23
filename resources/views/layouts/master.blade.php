6bcb79de25e0f852c91c5fafa16c6e460dd2082c
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-24 00:54:11 +0800

    Added the Inludes folder, bootstrap files, bootstrap header, and the login form

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,9 +2,10 @@
 <html>
     <head>
         <title>@yield('title')</title>
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
+		@include('includes.header')
         <div class="container">
 			@yield('content')
 		</div>
