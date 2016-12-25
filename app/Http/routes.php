e5468fe6cc13ee0dc56db67fdb7dd0bb156e1cf4
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-25 17:57:09 +0800

    added UserController.php and new create_users_table.php. modified welcome.blade.php, routes.php, and User.php. Auth folder deleted

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
+
+
+
+Route::group(['middleware' => ['web']], function() {
+	Route::get('/', function () {
     return view('welcome');
+	
+});
+	Route::post('/signup', ['uses' => 'UserController@postSignUp' , 'as' => 'signup']);
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 	<div class="row">
 		<div class="col-md-6">
 			<h3>Sign Up</h3>
-			<form action="#" method="post">
+			<form action="{{ route('signup') }}" method="post">
 				<div class="form-group">
 					<label for="email">Your E-Mail</label>
 					<input class="form-control" type="text" name="email" id="email">
@@ -22,6 +22,7 @@
 					<input class="form-control" type="password" name="password" id="password">
 				</div>
 				<button type="submit" class="btn btn-primary">Submit</button>
+				<input type="hidden" name="_token" value="{{ Session::token() }}">
 			</form>
 		</div>
 		<div class="col-md-6">
