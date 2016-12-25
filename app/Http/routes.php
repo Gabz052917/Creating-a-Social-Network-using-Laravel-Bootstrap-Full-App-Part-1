7cc7cba42e9a88420e4eb9e411c4de724824f353
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-26 00:30:58 +0800

    modified routes.php file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,6 +21,8 @@ Route::group(['middleware' => ['web']], function() {
 });
 	Route::post('/signup', ['uses' => 'UserController@postSignUp' , 'as' => 'signup']);
 	
+	Route::post('/signin', ['uses' => 'UserController@postSignIn' , 'as' => 'signin']);
+	
 	Route::get('/dashboard', [
 		'uses' => 'UserController@getDashboard',
 		'as' => 'dashboard'
