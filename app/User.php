a60be5d18a18ec37ebe805efbffdae6e3896308d
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-26 00:05:26 +0800

    Modified User.php

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
-    
+    use \Illuminate\Auth\Authenticatable;
 }
