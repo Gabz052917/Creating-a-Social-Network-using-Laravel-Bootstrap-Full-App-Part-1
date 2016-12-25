dcb0bedb13a7140a60f1df0529d07164553ca57f
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-26 00:29:00 +0800

    modified welcome.blade.php file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
 		</div>
 		<div class="col-md-6">
 			<h3>Sign In</h3>
-			<form action="#" method="post">
+			<form action="{{ route('signin') }}" method="post">
 				<div class="form-group">
 					<label for="email">Your E-Mail</label>
 					<input class="form-control" type="text" name="email" id="email">
