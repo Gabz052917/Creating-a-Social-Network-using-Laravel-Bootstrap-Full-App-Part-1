376239da0c10c3d7316a4535961f32977a8b3359
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-26 23:10:14 +0800

    modified the welcome.blade.php file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,15 @@
 
 @section('content')
 	@if(count($errors) > 0)
-	<div class="row">
-		<div class="col-md-6">
-			<ul>
-				@foreach($errors->all() as $error)
-					<li>{{$error}}</li>
-				@endforeach
-			</ul>
+		<div class="row">
+			<div class="col-md-6">
+				<ul>
+					@foreach($errors->all() as $error)
+						<li>{{$error}}</li>
+					@endforeach
+				</ul>
+			</div>
 		</div>
-	</div>
 	@endif
 	<div class="row">
 		<div class="col-md-6">
