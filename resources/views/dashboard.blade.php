8e1de94a54d35c3ea4b997358904383cafe35ee9
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-28 00:27:30 +0800

    modified Post.php, User.php, dashboard.blade.php file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
 			<header><h3>What do you have to say?</h3></header>
 			<form action="">
 				<div class="form-group">
-					<textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+					<textarea class="form-control" name="body" id="new-post" rows="5" placeholder="Your Post"></textarea>
 				</div>
 				<button type="submit" class="btn btn-primary">Create Post</button>
 			</form>
