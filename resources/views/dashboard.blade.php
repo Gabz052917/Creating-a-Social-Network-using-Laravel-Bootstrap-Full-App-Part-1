f7177209e8ea89cd82d2181adb0fc8f7047e1dc3
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-27 23:35:21 +0800

    modified dashboard.blade.php file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,10 +13,22 @@
 		</div>
 	</section>
 	<section class="row posts">
-		<div class="col-md-6 col-md-3-offset">
+		<div class="col-md-6 col-md-offset-3">
 			<header><h3>What other people say...</h3></header>
 			<article class="post">
-				<p>lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi </p>
+				<p>lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi repellat? Labore nemo nostrum, provident quam quasi velit? Accusamus aut beatae eius ex labore magni  minus obcaecati quia quisquam sit.</p>
+				<div class="info">
+					Posted by Max on 12 Feb 2016
+				</div>
+				<div class="interaction">
+					<a href="#">Like</a> |
+					<a href="#">Dislike</a> |
+					<a href="#">Edit</a> |
+					<a href="#">Delete</a>
+				</div>
+			</article>
+			<article class="post">
+				<p>lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi repellat? Labore nemo nostrum, provident quam quasi velit? Accusamus aut beatae eius ex labore magni  minus obcaecati quia quisquam sit.</p>
 				<div class="info">
 					Posted by Max on 12 Feb 2016
 				</div>
