56d6b524928b4389d93472c690b1603efc38fa2b
ahsirt101197 <banua.gabrielle10@gmail.com>
2016-12-28 15:41:03 +0800

    modified PostController.php, main.css, dashboard.blade.php, and welcome.blade.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,16 @@ class PostController extends Controller
 {
 	public function postCreatePost(Request $request)
 	{
+		$this->validate($request, [
+			'body' => 'required|max:1000'
+		]);
 		$post = new Post();
 		$post->body = $request['body'];
-		$request->user()->posts()->save($post);
-		return redirect()->route('dashboard');
+		$message = 'There was an error';
+		if ($request->user()->posts()->save($post))
+		{
+			$message = 'Post successfully created';
+		}
+		return redirect()->route('dashboard')->with(['message' => $message]);
 	}
 }
