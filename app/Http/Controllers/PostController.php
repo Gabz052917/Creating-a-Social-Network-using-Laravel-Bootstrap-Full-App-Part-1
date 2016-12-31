1440f1f5bc5ff53787e8ec4d207f2b89d8848309
ahsirt101197 <banua.gabrielle10@gmail.com>
2017-01-01 00:26:41 +0800

    modified PostController.php and dashboard.blade.php file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,13 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
 	public function getDashboard()
 	{
-		$posts = Post::all();
+		$posts = Post::orderBy('created_at', 'desc')->get();
 		return view('dashboard', ['posts' => $posts]);
 	}
 	public function postCreatePost(Request $request)
@@ -29,6 +30,9 @@ class PostController extends Controller
 	public function getDeletePost($post_id)
 		{
 			$post = Post::where('id', $post_id)->first();
+			if (Auth::user() != $post->user){
+				return redirect()->back();
+			}
 			$post->delete();
 			return redirect()->route('dashboard')->with(['message' => 'Successfully deleted!']);
 		}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,9 +25,13 @@
 				</div>
 				<div class="interaction">
 					<a href="#">Like</a> |
-					<a href="#">Dislike</a> |
+					<a href="#">Dislike</a> 
+					@if(Auth::user() == $post->user)
+
+					|
 					<a href="#">Edit</a> |
 					<a href="{{ route('post.delete', ['post_id' => $post->id]) }}">Delete</a>
+					@endif
 				</div>
 			</article>
             @endforeach
