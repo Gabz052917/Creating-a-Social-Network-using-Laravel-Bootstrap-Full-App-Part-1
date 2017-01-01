4b02c8aa18f87c22063755e5526b363c3ac0bc7c
ahsirt101197 <banua.gabrielle10@gmail.com>
2017-01-02 01:12:10 +0800

    modified dashboard.blade.php and master.blade.php file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,5 +10,12 @@
         <div class="container">
 			@yield('content')
 		</div>
+
+    <script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <script
+  src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body> 
 </html>
